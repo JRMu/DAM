2b86cbb8c9bfb914dd4e540dd88a363503cb9267
JRM <97874682+JRMu@users.noreply.github.com>
2024-10-06 13:05:06 +0200

    Terminado

---
+++ Programacion/ProgramacionA/Vt1ejercicio1/Vt1ejercicio1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Vt1ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escribir un programa donde se muestre por pantalla los números impares desde el 1 al 10.
+
+            int numero = 1;
+
+            while (numero <= 10)
+            {
+
+                Console.WriteLine(numero);
+                numero = numero + 2;
+
+            }
+
+            Console.WriteLine("El programa ha terminado.");
+
+        }
+    }
+}
---
+++ Programacion/ProgramacionA/Vt1ejercicio2/Vt1ejercicio2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Vt1ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hacer el diagrama de flujo de un programa que calcule el Factorial de un número N obtenido por consola (N! = 1*2*3....*N)
+            int n;
+            int contador;
+            int factorial = 1;
+
+            Console.WriteLine("Introduce el numero para calcular su Factorial:");
+            n= int.Parse(Console.ReadLine());
+
+            for(contador = 1; contador <= n; contador++)
+                factorial=factorial*contador;
+                
+                Console.WriteLine($"El resultado factorial de  {n}  es  {factorial}");
+
+        }
+    }
+}
