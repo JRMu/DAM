084f45f3617536daca989a2d37faf07a677349f1
JRM <97874682+JRMu@users.noreply.github.com>
2024-10-10 17:19:19 +0200

    Terminado

---
+++ Programacion/ProgramacionA/Suma1al5/Suma1al5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suma1al5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+
+
+            for (int contador = 1; contador <= 5; contador++)
+            {
+                suma += contador;
+            }
+            Console.WriteLine($"la suma de los numeros del 1 al 5 son {suma}");
+
+
+
+        }
+
+    }
+}
