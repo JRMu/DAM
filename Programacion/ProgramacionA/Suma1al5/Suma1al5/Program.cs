9c7f9c11bfea42385d1c3fc4754a6c6ac7b8a62f
JRM <97874682+JRMu@users.noreply.github.com>
2024-10-10 17:36:13 +0200

    comentario

--- Programacion/ProgramacionA/Suma1al5/Suma1al5/Program.cs
+++ Programacion/ProgramacionA/Suma1al5/Suma1al5/Program.cs
@@ -10,18 +10,20 @@ namespace Suma1al5
     {
         static void Main(string[] args)
         {
+            // Inicializamos la variable suma en 0 para almacenar la suma de los números.
             int suma = 0;
 
-
+            // Bucle for que se ejecuta desde 1 hasta 5, inclusive.
+            // La variable contador se incrementa en cada iteración.
             for (int contador = 1; contador <= 5; contador++)
             {
+                // En cada iteración, sumamos el valor de contador a la variable suma.
                 suma += contador;
             }
-            Console.WriteLine($"la suma de los numeros del 1 al 5 son {suma}");
 
-
-
+            // Mostramos el resultado de la suma en la consola.
+            Console.WriteLine($"La suma de los números del 1 al 5 es: {suma}");
         }
-
     }
 }
+
